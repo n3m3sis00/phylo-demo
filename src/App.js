c3bb34e1b7b80060a8a0ceebf5c8ede2e29415ea
Shreya Srivastava <ssrivastava@bt.iitr.ac.in>
2020-08-19 07:15:10 +0000

    Heading added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ function App() {
   const [draw, setDraw] = useState('none');
   const [showBranchLength] = useState(true);
   return (
-    <div className="App">
-      <button onClick={() => setDraw('fixed')}>Attach hacky fixed Tree MSA</button>
-      <button onClick={() => setDraw('buggy')}>Attach Tree MSA</button>
-      {draw === 'none' ? <h3>Click on any button above</h3> : null}
-      {draw === 'buggy' ? <TreeMSAB treedata={Lysine_treeNewickformat} msadata = {Lysine_msaStockformat} showBranchLength={showBranchLength} layout={"linear"}/> : null}
-      {draw === 'fixed' ? <TreeMSA treedata={Lysine_treeNewickformat} msadata = {Lysine_msaStockformat} showBranchLength={showBranchLength} layout={"linear"}/> : null}
+    <div>
+        <h1>Phylo React</h1> 
+        <div className="App">
+        <button onClick={() => setDraw('fixed')}>Attach hacky fixed Tree MSA</button>
+        <button onClick={() => setDraw('buggy')}>Attach Tree MSA</button>
+        {draw === 'none' ? <h3>Click on any button above</h3> : null}
+        {draw === 'buggy' ? <TreeMSAB treedata={Lysine_treeNewickformat} msadata = {Lysine_msaStockformat} showBranchLength={showBranchLength} layout={"linear"}/> : null}
+        {draw === 'fixed' ? <TreeMSA treedata={Lysine_treeNewickformat} msadata = {Lysine_msaStockformat} showBranchLength={showBranchLength} layout={"linear"}/> : null}
+        </div>
     </div>
   );
 }

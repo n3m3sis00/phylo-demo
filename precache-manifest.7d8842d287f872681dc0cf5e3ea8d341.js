7e30aadf2db11b038c0a69700e28ca9d1faf485e
Shreya Srivastava <ssrivastava@bt.iitr.ac.in>
2020-08-24 12:55:43 +0000

    Updates

---
+++ precache-manifest.7d8842d287f872681dc0cf5e3ea8d341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98ca2212b90c24202c85f861ae010aa",
+    "url": "/phylo-react/index.html"
+  },
+  {
+    "revision": "a3f6556ad811daca63de",
+    "url": "/phylo-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f38d8ebb5c8707dd7ca2",
+    "url": "/phylo-react/static/js/2.be328792.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/phylo-react/static/js/2.be328792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f6556ad811daca63de",
+    "url": "/phylo-react/static/js/main.2a65b889.chunk.js"
+  },
+  {
+    "revision": "602d0554dd0718de7720",
+    "url": "/phylo-react/static/js/runtime-main.e74e3694.js"
+  }
+]);
